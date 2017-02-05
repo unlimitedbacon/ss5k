5f285eb5818e86a6c860cfb4245c5b475bf7d835
Tyler Anderson <unlimitedbacon@gmail.com>
2017-02-05 13:21:12 -0800

    Generate form list only when app is actually run

--- app/forms.py
+++ app/forms.py
@@ -3,14 +3,18 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, PasswordField, BooleanField, SubmitField, SelectMultipleField
 from wtforms.validators import Required, Length, Email
 from app.models import User, Junkyard
+from app import app
 
 car_year_options = []
-for year in range(datetime.now().year,1886,-1):
-    car_year_options.append( (str(year),str(year)) )
-
 junkyard_options = []
-for yard in Junkyard.query.all():
-    junkyard_options.append( (str(yard.id),str("%s - %s" % (yard.state,yard.city))) )
+
+@app.before_first_request
+def create_form_options():
+    for year in range(datetime.now().year,1886,-1):
+        car_year_options.append( (str(year),str(year)) )
+
+    for yard in Junkyard.query.all():
+        junkyard_options.append( (str(yard.id),str("%s - %s" % (yard.state,yard.city))) )
 
 class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[Required(), Length(1, 256), Email()])
