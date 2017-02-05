aa94c1c8bdc3abbfe665fe420946a8ba891f3983
Tyler Anderson <unlimitedbacon@gmail.com>
2017-02-05 13:20:10 -0800

    Automatically create tmp directory

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,10 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from config import tmpdir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 
+# Create tmp directory if it doesn't exist
+if not os.path.exists(tmpdir):
+    os.makedirs(tmpdir)
+
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
